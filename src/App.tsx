b154efe6a1a4ddd83c3a208844775540b8a77ee9
JayceEse <jayce.ese@gmail.com>
2025-05-01 14:24:11 +1000

    create pokemon object & type

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
 
+// Pokemon Object
+const bulbasaur = {
+  id: 1,
+  name: 'Bulbasaur',
+  types: [{name: 'Grass'}],
+  sprite: {url:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'}
+  }
 
 function App() {
   return (
---
+++ src/types/PokemonTypes.ts
@@ -0,0 +1,8 @@
+
+// This type is used to define the structure of a single Pokemon object
+export type Pokemon = {
+    id: number,
+    name: string,
+    types: [{name: string}],
+    sprite: {url: string},
+    }
