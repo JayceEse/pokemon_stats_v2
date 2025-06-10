1492bc60812b51f4a64c3bd4f2f50ae6c8c3a168
JayceEse <jayce.ese@gmail.com>
2025-06-10 22:32:37 +1000

    fixing PokemonService

--- src/service/PokemonService.ts
+++ src/service/PokemonService.ts
@@ -1,10 +1,22 @@
 import axios from "axios";
-import { PokemonService } from "../types/PokemonTypes";
+import { PokemonListResult, PokemonService } from "../types/PokemonTypes";
 
 export const getManyPokemon = async (): Promise<PokemonService[]> => {
     try {
-        const {data} = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=5`)
-        return data.results
+        // Fetch the list of Pokemon
+        const listResponse = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=5`);
+        const pokemonList: PokemonListResult[] = listResponse.data.results;
+
+        // Fetch details for each Pokmeon
+        const pokemonDetails = await Promise.all(
+            pokemonList.map(async (pokmeon) => {
+                const detailsResponse = await axios.get(pokmeon.url);
+                return detailsResponse.data as PokemonService;
+            })
+        );
+
+        return pokemonDetails;
+
     } catch (error) {
         console.log(error)
         throw error
